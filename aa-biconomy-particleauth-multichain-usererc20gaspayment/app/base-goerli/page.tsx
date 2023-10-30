543f6246e9d2af11e0687be3474f4c6a533ca3bc
TechnoGeek01 <rsudharsangames@gmail.com>
2023-10-30 14:17:47 +0530

    updates

--- aa-biconomy-particleauth-multichain-usererc20gaspayment/app/base-goerli/page.tsx
+++ aa-biconomy-particleauth-multichain-usererc20gaspayment/app/base-goerli/page.tsx
@@ -245,7 +245,7 @@ const Minter: React.FC = () => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-52"
           onClick={connect}
         >
-          Connect metamask
+          Connect wallet
         </button>
         <button
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full w-52"
